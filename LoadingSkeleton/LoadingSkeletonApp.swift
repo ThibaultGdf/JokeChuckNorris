5ea1e402b92c08a3b2d6ccf79b438ecf538ade1c
ThibaultGdf <107555179+ThibaultGdf@users.noreply.github.com>
2024-02-28 10:35:40 +0100

    Initial Commit

---
+++ LoadingSkeleton/LoadingSkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingSkeletonApp.swift
+//  LoadingSkeleton
+//
+//  Created by Thibault GODEFROY on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoadingSkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
